da23969c9e7e25f63d80bf64586663840f65b81b
gabriel <gabriellopes112009@gmail.com>
2020-11-28 02:00:09 -0300

    Initia decoration of category_item

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:recepies_foods/models/category.dart';
 
 class CategoryItem extends StatelessWidget {
-  
   final Category category;
 
   const CategoryItem(this.category);
@@ -10,7 +9,19 @@ class CategoryItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text(category.title),    
+      child: Text(category.title),
+      padding: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        gradient: LinearGradient(
+          colors: [
+            category.color.withOpacity(0.5),
+            category.color
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight
+        ),
+      ),
     );
   }
 }

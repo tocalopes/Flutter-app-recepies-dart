1417acda2ac7e7afdecbb4aefe9a1748dcd03014
gabriel <gabriellopes112009@gmail.com>
2020-11-28 02:26:32 -0300

    navigator.push

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -1,25 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:recepies_foods/models/category.dart';
+import '../screens/categories_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   final Category category;
 
   const CategoryItem(this.category);
 
+  void _selectCategory(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (_){
+        return CategoriesMealsScreen();
+      },
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text(category.title,style: Theme.of(context).textTheme.headline6),
-      padding: EdgeInsets.all(15),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15),
-        gradient: LinearGradient(
-          colors: [
-            category.color.withOpacity(0.5),
-            category.color
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight
+    final containerRadius = BorderRadius.circular(15);
+    return InkWell(
+      borderRadius: containerRadius,
+      splashColor: Theme.of(context).primaryColor,
+      onTap: () {
+        _selectCategory(context);
+      },
+      child: Container(
+        child:
+            Text(category.title, style: Theme.of(context).textTheme.headline6),
+        padding: EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          borderRadius: containerRadius,
+          gradient: LinearGradient(
+              colors: [category.color.withOpacity(0.5), category.color],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight),
         ),
       ),
     );

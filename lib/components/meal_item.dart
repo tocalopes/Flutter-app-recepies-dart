5147f74baea8afd9a717c7d86b997a7c9ae8cc8a
gabriel <gabriellopes112009@gmail.com>
2020-11-28 11:09:11 -0300

    meal_item, img from url, ClipRRect to ser border

---
+++ lib/components/meal_item.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:recepies_foods/models/Meal.dart';
+
+class MealItem extends StatelessWidget {
+  final Meal meal;
+
+  const MealItem(this.meal);
+
+  void _selectMeal() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: _selectMeal,
+      child: Card(
+        elevation: 4,
+        margin: const EdgeInsets.all(10),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(5),
+        ),
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                ClipRRect( // input border
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(15),
+                    topRight: Radius.circular(15),
+                  ),
+                  child: Image.network(
+                    meal.imageUrl,
+                    height: 250,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:recepies_foods/components/meal_item.dart';
 import '../models/category.dart';
 import '../data/dummy_data.dart';
 
@@ -19,7 +20,7 @@ class CategoriesMealsScreen extends StatelessWidget {
       body: ListView.builder(
         itemCount: categoryMeals.length,
         itemBuilder: (context,index){
-          return Text(categoryMeals[index].title);
+          return MealItem(categoryMeals[index]);
         },
       ),
     );

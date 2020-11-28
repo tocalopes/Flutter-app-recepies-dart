1b3c33735056b7dc2f1e20c923ad727c60461e08
gabriel <gabriellopes112009@gmail.com>
2020-11-28 02:29:11 -0300

    categories meals with category on constructor

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -10,7 +10,7 @@ class CategoryItem extends StatelessWidget {
   void _selectCategory(BuildContext context) {
     Navigator.of(context).push(MaterialPageRoute(
       builder: (_){
-        return CategoriesMealsScreen();
+        return CategoriesMealsScreen(category);
       },
     ));
   }
--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
+import '../models/category.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
+  
+  final Category category;
+
+  const CategoriesMealsScreen(this.category);
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Receitas"),
+        title: Text(category.title),
       ),
       body: Center(
         child: Text(
-          "Receitas por categoria",
+          "Receitas por categoria ${category.id}",
         ),
       ),
     );
